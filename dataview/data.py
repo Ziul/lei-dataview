fd14abc6fa70704297611f1dd244bff09e72aa3b
Luiz Oliveira <ziuloliveira@gmail.com>
2015-01-11 00:57:42 -0300

    Read to be used to store some data

--- dataview/data.py
+++ dataview/data.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
+from scanf import sscanf
+
 
 class DataRead(object):
 
@@ -8,20 +10,30 @@ class DataRead(object):
     time = ''
     data = []
 
-    def __init__(self):
+    def __init__(self, arg=''):
         super(DataRead, self).__init__()
+        if len(arg) > 0:
+            self.read(arg)
 
     def read(self, loaded):
-        pass
+        self.time = sscanf(loaded, "%s\n%d %d %d\n%d %d %d\n%d %d %d\n")[0]
+        self.data = sscanf(loaded, "%s\n%d %d %d\n%d %d %d\n%d %d %d\n")[1:]
 
-from scanf import sscanf
-import sys
 
-data = ''
-for line in sys.stdin:
-    if line != '\n':
-        data += line
-    else:
-        print "lido: \n %s" % data,
-        print sscanf(data, "%s\n%d %d %d\n%d %d %d\n%d %d %d\n")
-        data = ''
+if __name__ == '__main__':
+
+    import sys
+
+    data = ''
+    store = []
+    # store = {}
+    datastore = DataRead(data)
+    for line in sys.stdin:
+        if line != '\n':
+            data += line
+        else:
+            datastore.read(data)
+            store.append(datastore.data)
+            # store[datastore.time] = datastore.data
+            data = ''
+    print(store)
