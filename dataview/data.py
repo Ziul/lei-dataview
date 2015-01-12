8889c4b4cefb59675fdbbb0c436049e9c30431f9
Luiz Oliveira <ziuloliveira@gmail.com>
2015-01-12 16:48:37 -0300

    Draft for plotting

--- dataview/data.py
+++ dataview/data.py
@@ -15,9 +15,23 @@ class DataRead(object):
         if len(arg) > 0:
             self.read(arg)
 
+    def setSerial(self, port):
+        self.serial = port
+
+    def __read(self):
+        line = self.serial.getline()
+        data = ''
+        while (line != '\n'):
+            data += line
+        return data
+
     def read(self, loaded):
         self.time = sscanf(loaded, "%s\n%d %d %d\n%d %d %d\n%d %d %d\n")[0]
         self.data = sscanf(loaded, "%s\n%d %d %d\n%d %d %d\n%d %d %d\n")[1:]
+        return self
+
+    def __str__(self):
+        return ("%s : " % self.time) + str(self.data)
 
 
 if __name__ == '__main__':
@@ -32,8 +46,9 @@ if __name__ == '__main__':
         if line != '\n':
             data += line
         else:
-            datastore.read(data)
+            ret = datastore.read(data)
             store.append(datastore.data)
             # store[datastore.time] = datastore.data
+            print ret.time
             data = ''
-    print(store)
+    # print(store)
--- dataview/plot.py
+++ dataview/plot.py
@@ -5,44 +5,95 @@ import matplotlib.pyplot as plt
 from threading import Thread
 from data import DataRead
 import signal
+import random
+import sys
 
 
 class Plotter(Thread):
 
     """docstring for Plotter"""
     alive = False
+    MAX = 50
 
     def __init__(self, readermethod):
         super(Plotter, self).__init__()
-        self.read = readermethod
+        self.readline = readermethod
         self.name = 'Plotter'
 
     def start(self):
         self.alive = True
+        signal.signal(signal.SIGINT, self.stop)
         super(Plotter, self).start()
 
     def stop(self):
         self.alive = False
 
     def run(self):
+        N_ADC = 10
+        fig = plt.figure(figsize=(16, 10))
+        axis = fig.add_subplot(111)
+        axis.set_xlim([0, self.MAX + 5])
+        plt.ion()
+        plt.show()
+        sys.stderr.write("Reading first data\n")
+        msg = self.readline()
+        msg = self.readline()
+        if (len(msg)) < N_ADC:
+            sys.stderr.write("Wating more sensors\n")
+            N_ADC = len(msg)
+        ADC = {"label": [], 'values': [], 'axis': range(N_ADC)}
+        for i in range(N_ADC):
+            ADC['label'].append("Sensor_" + str(i))
+            ADC['values'].append([]
+                                 )
+            ADC['axis'][i], = (axis.plot(ADC['values'][i]))
+        fig.legend(ADC['axis'], ADC['label'])
         while self.alive:
-            self.read()
+            try:
+                msg = self.readline()
+                for i in range(N_ADC):
+                    ADC['values'][i].append(float(msg[i]))
+                    if(len(ADC['values'][i]) > self.MAX):
+                        ADC['values'][i] = ADC['values'][i][1:]
+                    ADC['axis'][i].set_ydata(ADC['values'][i])
+                    ADC['axis'][i].set_xdata(range(len(ADC['values'][i])))
+                    axis.relim()
+                    axis.autoscale_view(True, True, True)
+                # print msg
+            except ValueError:
+                sys.stderr.write(
+                    "Dado mal formatado " + str(msg) + "\n")
+            except IndexError:
+                sys.stderr.write("\rÍndice " + str(i) + " inexistente\n")
+                pass
+            except TypeError:
+                print "\r No new value %s" % str(msg),
+                self.stop()
+            plt.draw()
 
 
 def signal_handler(signal, frame):
-    import sys
+
     sys.stderr.write('\nCtrl+C pressionado!\n\n')
     p.stop()
 
 
-def f():
-    print('oi')
+def f(arg=0):
+    # print('oi')
+    data = ''
+    datastore = DataRead(data)
+    for line in sys.stdin:
+        if line != '\n':
+            data += line
+        else:
+            ret = datastore.read(data)
+            return ret.data
 
 
 def main():
     signal.signal(signal.SIGINT, signal_handler)
     p.start()
-    p.join(10)
+    p.join()
 
 
 if __name__ == '__main__':
