10f83f5ee423c9e7506957d867b74c49a5d09fbc
Luiz Oliveira <ziuloliveira@gmail.com>
2015-01-25 17:01:58 -0200

    Update tester

--- test.py
+++ test.py
@@ -9,122 +9,67 @@ Test the speed of rapidly updating multiple plot curves
 
 
 from pyqtgraph.Qt import QtGui, QtCore
-import numpy as np
 import pyqtgraph as pg
-from pyqtgraph.ptime import time
-
-import serial
-from dataview.device import util
-from dataview.data import DataRead
-# QtGui.QApplication.setGraphicsSystem('raster')
-app = QtGui.QApplication([])
-#mw = QtGui.QMainWindow()
-# mw.resize(800,800)
-
-p = pg.plot()
-p.setWindowTitle('DataView')
-#p.setRange(QtCore.QRectF(0, -10, 5000, 20))
-p.setLabel('bottom', 'Index', units='B')
-
-nPlots = 10
-nSamples = 500
-#curves = [p.plot(pen=(i,nPlots*1.3)) for i in range(nPlots)]
-curves = []
-for i in range(nPlots):
-    c = pg.PlotCurveItem(pen=(i, nPlots * 1.3))
-    p.addItem(c)
-    c.setPos(0, i * 6)
-    curves.append(c)
-
-p.setYRange(0, nPlots * 6)
-p.setXRange(0, nSamples)
-p.resize(600, 900)
-
-rgn = pg.LinearRegionItem([nSamples / 5., nSamples / 3.])
-p.addItem(rgn)
+import sys
+import numpy as np
+import signal
+import random
 
 
-# data = np.random.normal(size=(nPlots * 23, nSamples))
-data = []
-ptr = 0
-lastTime = time()
-fps = None
-count = 0
-uc = object()
-
-
-def read_data_uc():
-    global uc, nPlots
-    data = ''
-    datastore = DataRead(data)
-    while True:
-        line = uc.readline()
-        if line != '\n':
-            data += line
-        else:
-            ret = datastore.read(data)
-            nPlots = len(ret.data)
-            return ret
-
-
-def update():
-    global curve, data, ptr, p, lastTime, fps, nPlots, count, uc
-    count += 1
-    # print "---------", count
-    data.append(read_data_uc().data)
-    for i, d in zip(range(nPlots), data):
-        curves[i].setData(d)
-
-    # print "   setData done."
-    ptr += nPlots
-    now = time()
-    dt = now - lastTime
-    lastTime = now
-    if fps is None:
-        fps = 1.0 / dt
-    else:
-        s = np.clip(dt * 3., 0, 1)
-        fps = fps * (1 - s) + (1.0 / dt) * s
-    p.setTitle('%0.2f fps' % fps)
-    # app.processEvents()  ## force complete redraw for every plot
-
-
-def signal_handler(signal, frame):
+def signal_handler(signal=0, frame=0):
 
     sys.stderr.write('\nCtrl+C  pressed!\n\n')
     exit()
 
-# Start Qt event loop unless running in interactive mode.
-if __name__ == '__main__':
-    import sys
-    import signal
-    signal.signal(signal.SIGINT, signal_handler)
+color = 2
+b = range(10)
 
-    # list available ports
-    print 'Available ports:'
-    PORTS_AVAILABLE = util.available_ports()
-    try:
-        for k, i in zip(PORTS_AVAILABLE, range(len(PORTS_AVAILABLE))):
-            print '%d >>> %s' % (i, k)
-        print '---'
-    except TypeError, error:
-        print "None device connected"
-        exit()
 
-    # choose a port
-    if len(PORTS_AVAILABLE) == 1:
-        uc = microcontroller(PORTS_AVAILABLE[0])
-    elif len(PORTS_AVAILABLE) == 0:
-        print "None device connected"
-        exit()
-    elif len(PORTS_AVAILABLE) > 1:
-        choosed = input("Choose one:")
-        print "\nChoosed: %s" % PORTS_AVAILABLE[int(choosed)]
-        uc = serial.Serial(PORTS_AVAILABLE[int(choosed)], 9600, timeout=0)
+def update():
+    global color, b
+    a = random.randint(10, 200)
+    b.append(a)
+    if len(b) > 30:
+        b = b[1:]
+    p.setData(b[10:])
 
-    timer = QtCore.QTimer()
-    timer.timeout.connect(update)
-    timer.start(0)
 
-    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-        QtGui.QApplication.instance().exec_()
+def includer():
+    win.nextRow()
+    x = range(10)
+    y = range(10)
+    curve1 = win.addPlot(title='teste')
+    curve1.plot(x, y)
+
+app = QtGui.QApplication([])
+# mw = QtGui.QMainWindow()
+# mw.resize(800, 800)
+win = pg.GraphicsWindow()
+win.setWindowTitle('Scrolling Plots')
+
+x = range(10)
+y = range(10)
+plotWidget = win.addPlot()
+p = plotWidget.plot(x, y)
+
+win.nextRow()
+curve1 = win.addPlot()
+curve1.plot(x, y)
+
+
+timer = QtCore.QTimer()
+timer.timeout.connect(update)
+timer.start(100)
+
+inc = QtCore.QTimer()
+inc.timeout.connect(includer)
+inc.start(3000)
+
+
+quiter = QtCore.QTimer()
+quiter.timeout.connect(signal_handler)
+quiter.start(7000)
+
+signal.signal(signal.SIGINT, signal_handler)
+if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+    QtGui.QApplication.instance().exec_()
