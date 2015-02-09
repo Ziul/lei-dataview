46231d4143396012b38fab7face113cb7e5ea838
Luiz Oliveira <ziuloliveira@gmail.com>
2015-02-09 15:28:22 -0200

    Draft of the new devices

---
+++ dataview/device.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+try:
+    from smbus import SMBus
+except ImportError:
+    print "smbus not installed"
+
+try:
+    from serial.tools.miniterm import Miniterm, CONVERT_CR, CONVERT_CRLF, CONVERT_LF, EXITCHARCTER, MENUCHARACTER
+    from serial.tools.list_ports import comports
+except Exception as e:
+    if float(serial.VERSION) < 2.6:
+        print 'Upgrade Pyserial'
+    raise e
+try:
+    from glob import glob
+except Exception as e:
+    print 'Install glob'
+    raise e
+
+
+ROOT_MESSAGE = """
+  .-------------------------------.
+ ( You need Super Cow Powers here. )
+  '-------------------------------'
+         \   ^__^
+          \  (oo)\_______
+             (__)\       )\/\\
+                 ||----w |
+                 ||     ||
+
+"""
+
+
+class I2C_Sensor(object):
+
+    """Basic definitions of a sensor"""
+
+    address = 0x00
+    port = 0
+    mode = 0
+    values = []
+    live = False
+
+    def __init__(self, port, address, mode=0):
+        """ Method to initialize the sensor """
+        self.address = address
+        self.port = port
+        self.mode = mode
+
+        try:
+            self.bus = SMBus(self.port)
+        except IOError:
+            self.bus = None
+            self.live = False
+        except Exception as e:
+            raise e
+
+    def enable(self):
+        """ Method to enable the sensor """
+
+        # if aready got a bus for it
+        if self.bus:
+            self.live = True
+
+    def disable(self):
+        """ Method to desable the sensor """
+        self.live = False
+
+    def acquire(self, cmd=0):
+        """ Method to acquire the values from sensor """
+        if self.live:
+            return self.bus.read_i2c_block_data(self.address, cmd)
+
+    def __str__(self):
+        if self.bus:
+            self.acquire()
+            return (
+                "Sensor at i2c-%d: 0x%x - %s" % (
+                    self.port, self.address, self.values)
+            )
+        else:
+            return "Sensor not ready"
+
+
+class Serial_Sensor(Miniterm):
+
+    def __init__(
+        self,
+        port,
+        baudrate=9600,
+        parity='N',
+        rtscts=False,
+        xonxoff=False,
+        echo=False,
+        convert_outgoing=CONVERT_CRLF,
+        mode=0):
+                super(
+                    Serial_Sensor,
+                    self).__init__(port,
+                                   baudrate,
+                                   parity,
+                                   rtscts,
+                                   xonxoff,
+                                   echo=False,
+                                   convert_outgoing=CONVERT_CRLF,
+                                   repr_mode=mode)
+
+    def enable(self):
+        """ Method to enable the sensor """
+        self.alive = True
+
+    def disable(self, signal=None, frame=None):
+        """ Method to desable the sensor """
+        self.alive = False
+        self.serial.close()
+
+    def acquire(self):
+        """ Method to acquire the values from sensor """
+        if self.alive:
+            return self.serial.readline()
+
+    def read(self):
+        pass
+
+
+if __name__ == '__main__':
+    available = []
+    available += glob('/dev/ttyUSB*') + glob('/dev/ttyACM*')
+    try:
+        for port, desc, hwid in sorted(comports()):
+            if port not in port:
+                available.append(port)
+    except Exception as e:
+        raise e
+    print available
