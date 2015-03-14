0cfb526f17325440e01870f88afee91daa6e53a6
Luiz Oliveira <ziuloliveira@gmail.com>
2015-03-14 11:27:30 -0300

    new test modele
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

--- test.py
+++ test.py
@@ -15,31 +15,57 @@ import numpy as np
 import signal
 import random
 
+import serial
+from dataview.scanf import sscanf
+from dataview.scanf import IncompleteCaptureError
+
+color = 2
+x = range(10)
+y = range(10)
+z = range(10)
+tty = serial.Serial(timeout=0)
 
 def signal_handler(signal=0, frame=0):
 
     sys.stderr.write('\nCtrl+C  pressed!\n\n')
     exit()
 
-color = 2
-b = range(10)
-
-
 def update():
-    global color, b
-    a = random.randint(10, 200)
-    b.append(a)
-    if len(b) > 30:
-        b = b[1:]
-    p.setData(b[10:])
+    global color, tty,x,y,z
+    formatter = "x1:  %d y1:  %d z1:  %d"
+    #a = random.randint(10, 200)
+    loaded = tty.readline()
+    try:
+        a= sscanf(loaded, formatter)
+        x.append(a[0])
+        y.append(a[1])
+        z.append(a[2])
+        print a
+    except IncompleteCaptureError:
+        #print loaded
+        pass
+    except Exception, e:
+        raise e
+    
+    if len(x) > 100:
+        x = x[1:]
+    p.setData(x)
+
+    if len(y) > 100:
+        y = y[1:]
+    q.setData(y)
 
 
-def includer():
-    win.nextRow()
-    x = range(10)
-    y = range(10)
-    curve1 = win.addPlot(title='teste')
-    curve1.plot(x, y)
+    if len(z) > 100:
+        z = z[1:]
+    r.setData(z)
+
+
+
+tty.baudrate = 9600
+tty.port = '/dev/ttyACM0'
+tty.open()
+
 
 app = QtGui.QApplication([])
 # mw = QtGui.QMainWindow()
@@ -49,26 +75,31 @@ win.setWindowTitle('Scrolling Plots')
 
 x = range(10)
 y = range(10)
-plotWidget = win.addPlot()
-p = plotWidget.plot(x, y)
+plotWidgetx = win.addPlot()
+plotWidgetx.setRange(yRange=[300, 550])
+p = plotWidgetx.plot(x, y)
 
 win.nextRow()
-curve1 = win.addPlot()
-curve1.plot(x, y)
+plotWidgety = win.addPlot()
+plotWidgety.setRange(yRange=[300, 550])
+q = plotWidgety.plot(x, y)
+
+win.nextRow()
+plotWidgetz = win.addPlot()
+plotWidgetz.setRange(yRange=[300, 550])
+r = plotWidgetz.plot(x, y)
+
+
+
 
 
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
-timer.start(100)
-
-inc = QtCore.QTimer()
-inc.timeout.connect(includer)
-inc.start(3000)
-
+timer.start(0)
 
 quiter = QtCore.QTimer()
 quiter.timeout.connect(signal_handler)
-quiter.start(7000)
+#quiter.start(7000)
 
 signal.signal(signal.SIGINT, signal_handler)
 if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
